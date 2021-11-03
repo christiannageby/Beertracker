5b68cda9b0bbb8e5a4b4f0db6c366cc591baff28
chna <christian.nageby@mil.se>
2021-11-03 18:03:18 +0100

    add docstring

--- beertracker.py
+++ beertracker.py
@@ -1,15 +1,11 @@
-from flask import Flask, render_template, request, flash
-from werkzeug.utils import redirect
+"""Main application document for importing all models and blueprints"""
+from flask import Flask
 from shared import db
 from blueprints.recipes import recipe_actions
 from blueprints.brews import brew_actions
-
-
-import sqlalchemy.exc
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 app.register_blueprint(recipe_actions)
 app.register_blueprint(brew_actions)
 db.init_app(app)
-
