2b75f47a13091950fb08621ff122e7b4a0b61e19
chna <christian.nageby@mil.se>
2021-11-05 15:38:02 +0100

    send brews_per_months variable for a future brew graph

--- beertracker/blueprints/dashboard.py
+++ beertracker/blueprints/dashboard.py
@@ -1,6 +1,7 @@
 """Actions for brews"""
 from datetime import datetime
-from flask import Blueprint, request, redirect, flash, render_template
+from flask import Blueprint, render_template
+from sqlalchemy import func
 from sqlalchemy.exc import SQLAlchemyError, NoResultFound
 from beertracker.models.brew import Brew
 from beertracker.models.recipe import Recipe
@@ -10,6 +11,7 @@ dashboard_actions = Blueprint('dashboard_actions', __name__)
 
 @dashboard_actions.route('/')
 def dashboard() -> render_template:
+    brews_per_month = db.session.query(func.strftime('%Y-%m', Brew.brew_day), func.count(Brew.id)).group_by(func.strftime('%m', Brew.brew_day)).limit(12).all()
     today = datetime.now().date()
     fermenting_brews = db.session.query(Brew, Recipe).join(Recipe).filter(Brew.recipe_id == Recipe.id and today >= Brew.brew_done_ferm).order_by(Brew.brew_day.desc())
-    return render_template('dashboard.html', fermenting_brews=fermenting_brews, today=today)
+    return render_template('dashboard.html', fermenting_brews=fermenting_brews, today=today, monthly_chart=brews_per_month)
